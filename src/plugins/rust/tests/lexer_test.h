71873e30a6553151372cdb46461c79e376568c98
Michał Małek <michalm@fastmail.fm>
2017-04-20 17:35:05 +0200

    Started writing Rust lexer for highlighting/commenting/bracket matching

---
+++ src/plugins/rust/tests/lexer_test.h
@@ -0,0 +1,53 @@
+/****************************************************************************
+**
+** Copyright (C) Michal Malek <michalm@fastmail.fm>
+** Contact: http://www.qt.io/licensing
+**
+** This file is part of Qt Creator.
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+****************************************************************************/
+
+#pragma once
+
+#include <QObject>
+
+namespace Rust {
+namespace Internal {
+
+class LexerTest : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void empty();
+    void state();
+    void whitespace();
+    void identifier();
+    void keyword();
+    void integer1();
+    void integer2();
+    void integer3();
+    void integer4();
+    void floating1();
+    void floating2();
+    void floating3();
+};
+
+} // namespace Internal
+} // namespace Rust
---
+++ src/plugins/rust/token.h
@@ -0,0 +1,57 @@
+/****************************************************************************
+**
+** Copyright (C) Michal Malek <michalm@fastmail.fm>
+** Contact: http://www.qt.io/licensing
+**
+** This file is part of Qt Creator.
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+****************************************************************************/
+
+#pragma once
+
+namespace Rust {
+namespace Internal {
+
+enum class TokenType {
+    None,
+    Keyword,
+    Operator,
+    Identifier,
+    String,
+    Number,
+    Symbol,
+    Comment
+};
+
+struct Token
+{
+    int begin;
+    int length;
+    TokenType type;
+
+    bool operator==(const Token &rhs) const
+    {
+        return begin == rhs.begin && length == rhs.length && type == rhs.type;
+    }
+
+    Token() = delete;
+};
+
+} // namespace Internal
+} // namespace Rust
